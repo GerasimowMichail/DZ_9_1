db8d6a37fb82e2e1f8c2d26638f20de91645c28b
Mischa <GerasimowMichail@yandex.ru>
2022-11-27 18:14:49 +0300

    ДЗ готово

--- ZD_66/Program.cs
+++ ZD_66/Program.cs
@@ -1,2 +1,24 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 66: Задайте значения M и N. Напишите программу, которая найдёт 
+// сумму натуральных элементов в промежутке от M до N.
+
+// M = 1; N = 15 -> 120
+// M = 4; N = 8. -> 30
+
+
+Console.Write("Введите начало интервала N ");
+int n = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите начало интервала M ");
+int m = Convert.ToInt32(Console.ReadLine());
+
+int SumDigits(int n, int m)
+{
+    int sum = 0;
+    while (n <= m)
+    {
+        sum = sum + n;
+        n++;
+    }
+    return sum;
+}
+Console.WriteLine(SumDigits(n,m));
+
--- ZD_68/Program.cs
+++ ZD_68/Program.cs
@@ -1,2 +1,26 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 68: Напишите программу вычисления функции Аккермана с помощью рекурсии. 
+// Даны два неотрицательных числа m и n.
+// m = 2, n = 3 -> A(m,n) = 9
+// m = 3, n = 2 -> A(m,n) = 29
+
+
+
+Console.Write("Введите начало интервала N ");
+int n = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите начало интервала M ");
+int m = Convert.ToInt32(Console.ReadLine());
+
+int Akerman(int n, int m)
+{
+   {
+  if (n == 0)
+    return m + 1;
+  else
+    if ((n != 0) && (m == 0))
+      return Akerman(n - 1, 1);
+    else
+      return Akerman(n - 1, Akerman(n, m - 1));
+}
+}
+Console.WriteLine(Akerman(n,m));
+
